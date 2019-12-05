0cf9705c2dcbf742dbaf2b05209f77344e1be9ee
vtechguys <vtechguys@gmail.com>
2019-12-05 20:29:08 +0530

    route maping bug

--- express-min/router/route.js
+++ express-min/router/route.js
@@ -1,16 +1,7 @@
 'use strict'
-
+const url = require('url');
 function route(handle, pathname, request, response){
-    let compeltePathNameIncludingMethod = pathname;
-    
-    if (request.method === "GET") {
-        compeltePathNameIncludingMethod + '_get';
-    }
-    else if(request.method === "POST"){
-        compeltePathNameIncludingMethod + '_post';
-
-    }
-
+    let compeltePathNameIncludingMethod = pathname + '_' + url.parse( request.url ).pathname.toUpperCase();
 
     if(typeof handle[compeltePathNameIncludingMethod]!== "function"){
         throw new Error('Request handler required for pathname' + pathname);
