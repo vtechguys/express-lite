be7552f6ce7036741cc4201acee61e19189fc25c
vtechguys <vtechguys@gmail.com>
2019-12-05 20:21:55 +0530

    logs improvement

--- router/check-log.js
+++ router/check-log.js
@@ -1,9 +1,23 @@
 const express = require('../express-min');
 const router = express.Router();
 
-router.get('/log', (request, response)=>{
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('hello world');
-    response.end();
+const fs = require('fs');
+const path = require('path');
+
+router.get('/log', (request, response) => {
+    const fileName = 'http_logger.log';
+    const filePath = path.join(__dirname, './../', './logs', fileName);
+    fs.exists(filePath, function (exists) {
+        if (exists) {
+            response.writeHead(200, {
+                "Content-Type": "text/plain",
+            });
+            fs.createReadStream(filePath).pipe(response);
+        } else {
+            response.writeHead(400, { "Content-Type": "text/plain" });
+            response.write('ERROR file doest not exist');
+            response.end();
+        }
+    });
 });
 module.exports = router;
--- utils/httpLogger/index.js
+++ utils/httpLogger/index.js
@@ -1,7 +1,8 @@
 'use strict'
 const logModule = require('./log');
+const url = require('url');
 function httpLogger(request, response, pathToSaveLogFile){
-
-    logModule(pathToSaveLogFile, 'http_logger', `Request recived for pathname ${request.url}`);
+    const urlLog = url.parse(request.url).pathname;
+    logModule(pathToSaveLogFile, 'http_logger', `Requested pathname ${urlLog}`);
 }
 module.exports = httpLogger;
